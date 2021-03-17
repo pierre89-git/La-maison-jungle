3c6677a62972399154e51790ccf0e8c1880d6cc4
Pierre Jutand <pierre.jutand@gmail.com>
2021-03-17 11:29:20 +0100

    Ajout de state class Cart

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import Banner from "./Banner";
-//import Cart from "./Cart"
+import Cart from "./Cart"
 import ShoppingList from "./ShoppingList"
 import Footer from './Footer'
+import '../styles/Layout.css'
+
 
 function App() {
 	return <div>
 						<Banner />
 						{/*<Cart />*/}
+						<div className='lmj-layout-inner'>
+						<Cart />
 						<ShoppingList />
+						</div>
 						<Footer />
 
 					</div>
